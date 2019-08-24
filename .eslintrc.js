2fa9aeeb709ea4d17df23da1f43affa03b7c383a
Don Osipov <48395550+Don-Osipov@users.noreply.github.com>
2019-08-24 17:56:08 -0400

    Init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    'plugin:vue/strongly-recommended',
+    '@vue/prettier'
+  ],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    indent: ['warn', 2],
+    'no-unused-vars': 'warn'
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  }
+};
